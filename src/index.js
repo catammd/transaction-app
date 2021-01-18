2885d0138466cb5bd0e326e616686cb11725ca5f
Catalin Stefan Tatu <catalin.tatu@creuna.dk>
2021-01-18 18:13:20 +0100

    Clean-up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
-import App from './Components/App';
+import App from "./Components/App";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -13,18 +13,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById("root")
 );
-
-// See a list of transactions with the most relevant data for the customer
-
-/**
- * TODO:
- * Get the transactions from the API https://cdn.prod.lunarway.com/voyager/transactions_test_dev.json
- * Render a list with items from api list
- * Style the list a little bit
- */
-
-// Clicking on a item list will show details for that transaction
-
-// The list can be filtered. For example, only see transactions with a specific status, type, etc.
-
-// Add posibility to delete a transaction- it's fine if the transaction re-appears on page load. 
